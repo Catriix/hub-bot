7b6c49843d0892da0778f3c19323e5a01ba307a4
Catriix <catriix.contact@gmail.com>
2021-02-04 19:16:06 +0100

    Added prune command

---
+++ commands/prune.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: 'prune',
+  description: 'Prune up to 99 messages',
+  execute(message, args) {
+    const amount = parseInt(args[0]) + 1;
+
+    if(isNaN(amount)) {
+      return message.reply('that doesn\'t seem to be a valid number.');
+    } else if (amount <= 1 || amount > 99) {
+      return message.reply('enter a number between 1 and 99.');
+    }
+
+    message.channel.bulkDelete(amount, true).catch(err => {
+      console.error(err);
+      message.channel.send('an error occured while trying to prune messages in this channel.');
+    });
+  },
+};
